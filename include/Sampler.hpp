240c666c83edef2d11082478fdddcedfb1560180
charlesfelix <charlesfelix@gmail.com>
2018-04-16 12:17:47 -0700

    Added XorShift RNG
    Added SamplerXorS
    Also removed the use of static variable for keep track of the state

--- include/Sampler.hpp
+++ include/Sampler.hpp
@@ -13,11 +13,12 @@
 
 namespace Fr
 {
-    class Sampler
+    
+    class SamplerS
     {
     public:
-        Sampler(unsigned long seed = 11212);
-        Sampler(const Sampler & sampler):m_seed(sampler.m_seed+3),m_random(FR_RAND48(sampler.m_seed+3)){}
+        SamplerS(unsigned long seed = 11212);
+        SamplerS(const SamplerS & sampler):m_seed(sampler.m_seed+3),m_random(FR_RAND48(sampler.m_seed+3)){}
         V3f sampleUnitSphere() const;
         float random() const;
     private:
@@ -25,5 +26,18 @@ namespace Fr
         mutable FR_RAND48 m_random;
     };
     
+    class SamplerXorS
+    {
+    public:
+        SamplerXorS(unsigned int seed = 11212);
+        V3f sampleUnitSphere() const;
+        float random() const;
+    private:
+        mutable unsigned int m_state;
+    };
+    
+    typedef SamplerXorS Sampler;
+    
+
 } // namespace Fr
 #endif /* Sampler_hpp */
--- src/Sampler.cpp
+++ src/Sampler.cpp
@@ -9,12 +9,23 @@
 #include "Sampler.hpp"
 using namespace Fr;
 
-Sampler::Sampler(unsigned long seed )
+static unsigned int XorShift32(unsigned int& state)
+{
+    uint32_t x = state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 15;
+    state = x;
+    return x;
+}
+
+SamplerS::SamplerS(unsigned long seed )
 {
     m_random = FR_RAND48( seed );
 }
+float SamplerS::random() const { return m_random.nextf() ; };
 
-V3f Sampler::sampleUnitSphere() const
+V3f SamplerS::sampleUnitSphere() const
 {
     V3f p;
     do
@@ -24,4 +35,19 @@ V3f Sampler::sampleUnitSphere() const
     return p;
 }
 
-float Sampler::random() const { return m_random.nextf() ; };
+SamplerXorS::SamplerXorS(unsigned int state):m_state(state) {}
+
+float SamplerXorS::random() const {
+    return (XorShift32(m_state) & 0xFFFFFF) / 16777216.0f;
+}
+
+V3f SamplerXorS::sampleUnitSphere() const
+{
+    V3f p;
+    do
+    {
+        p = 2.f*V3f(random(),random(),random())-V3f(1.f,1.f,1.f);
+    } while (p.length2()>=1.0);
+    return p;
+}
+
